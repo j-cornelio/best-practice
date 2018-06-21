52f441115095937f048a77ddcc583fb4e32cec80
julio <empjulio@hotmail.com>
2018-06-21 19:47:42 -0400

    refactored a lil, Todo

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -3,9 +3,12 @@ import React                   from 'react'
 import { connect }             from 'react-redux'
 import { handleVisibility }    from '../../actions/'
 
+//presentational component, don't specify behavior
 const Todo = ({ text, completed, id, handleToggleTodo }) => (
-  <li onClick={
+  <li 
+      onClick={
         handleToggleTodo.bind(this, id)
+        //handleToggleTodo
       }
       style={{textDecoration: completed ? 'line-through' : 'none'}}
     >
@@ -13,10 +16,15 @@ const Todo = ({ text, completed, id, handleToggleTodo }) => (
     </li>
 )//
 
+// acts as container component - 20
 const TodoList = ({ visibleTodos, handleToggleTodo }) => (
   <ul>
     { visibleTodos.map( todo => (
-        <Todo key={todo.id} handleToggleTodo={handleToggleTodo} {...todo} />
+        <Todo 
+          key={todo.id} 
+          handleToggleTodo={handleToggleTodo}
+          // handleToggleTodo={() => handleToggleTodo(todo.id)}  
+          {...todo} />
       )) 
     }
   </ul>
